acd41159a9138cc0b968d424c22216e9e58e62dc
nguyenlinh189 <91927899+nguyenlinh189@users.noreply.github.com>
2022-10-12 10:17:25 +0700

    Add files via upload

---
+++ btl/src/User.java
@@ -0,0 +1,47 @@
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class User implements Serializable{
+    private String username,password,server;
+
+    public User(String username, String password,String server) {
+        this.username = username;
+        this.password = password;
+        this.server=server;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+    
+}
